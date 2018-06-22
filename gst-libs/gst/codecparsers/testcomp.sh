84c16db6922974cafe047a2b747330fa4f63cf1d
ottingerg <g.ottinger@gmx.at>
2018-06-22 10:05:54 +0200

    added simple compilescript for typo hunting

---
+++ gst-libs/gst/codecparsers/testcomp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc gstav1parser.c -o dummy -I/home/imedia/SourceCode/gstreamer/gstreamer/libs -I/home/imedia/SourceCode/gstreamer/gstreamer -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/ -I/home/imedia/SourceCode/gstreamer/gstreamer/build -I/home/imedia/SourceCode/gsoc/gst-plugins-bad/gst-libs -DGST_USE_UNSTABLE_API
